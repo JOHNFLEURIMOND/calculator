28946461ca4a296a33306dea2a44da20a5168ab3
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-03-05 01:07:08 -0500

    Refactored Calculator

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ LIC:
 //what can user see.
 //what can a user expect.
 
-
 //User can type two numbers and select a operator.
 //user can see two input form  and four buttons
 // User will select from four buttons.
@@ -14,73 +13,72 @@ LIC:
 //User can expect see one result after the two numbers are added, subtracted, divided, multiplied.
 //and the results will be displayed in the display bar
 
-document.getElementById("plus").onclick = add;
-document.getElementById('minus').onclick = minus;
-document.getElementById('mult').onclick = mult;
-document.getElementById('divide').onclick = divide;
-
+document.getElementById("plus").onclick = add
+document.getElementById("minus").onclick = minus
+document.getElementById("mult").onclick = mult
+document.getElementById("divide").onclick = divide
 
 function add() {
   //get value out of inputs
-  var num1 = parseFloat(document.getElementById('firstVal').value)
-  var num2 = parseFloat(document.getElementById('secondVal').value)
+  var num1 = parseFloat(document.getElementById("firstVal").value)
+  var num2 = parseFloat(document.getElementById("secondVal").value)
 
   var sum = num1 + num2
   //user sees result
   //display
 
-  document.getElementById('result').innerHTML = sum
+  document.getElementById("result").innerHTML = sum
 
-  document.getElementById('firstVal').innerHTML = ""
-  document.getElementById('secondVal').innerHTML = ""
-
+  document.getElementById("firstVal").innerHTML = ""
+  document.getElementById("secondVal").innerHTML = ""
 }
-
 
 function minus() {
   //get value out of inputs
-  var num1 = parseFloat(document.getElementById('firstVal').value)
-  var num2 = parseFloat(document.getElementById('secondVal').value)
+  var num1 = parseFloat(document.getElementById("firstVal").value)
+  var num2 = parseFloat(document.getElementById("secondVal").value)
 
   var sum = num1 - num2
   //user sees result
   //display
 
-  document.getElementById('result').innerHTML = sum
+  document.getElementById("result").innerHTML = sum
 
-  document.getElementById('firstVal').innerHTML = ""
-  document.getElementById('secondVal').innerHTML = ""
-
+  document.getElementById("firstVal").innerHTML = ""
+  document.getElementById("secondVal").innerHTML = ""
 }
 
 function mult() {
   //get value out of inputs
-  var num1 = parseFloat(document.getElementById('firstVal').value)
-  var num2 = parseFloat(document.getElementById('secondVal').value)
+  var num1 = parseFloat(document.getElementById("firstVal").value)
+  var num2 = parseFloat(document.getElementById("secondVal").value)
 
   var sum = num1 * num2
   //user sees result
   //display
 
-  document.getElementById('result').innerHTML = sum
+  document.getElementById("result").innerHTML = sum
 
-  document.getElementById('firstVal').innerHTML = ""
-  document.getElementById('secondVal').innerHTML = ""
-
+  document.getElementById("firstVal").innerHTML = ""
+  document.getElementById("secondVal").innerHTML = ""
 }
 
 function divide() {
   //get value out of inputs
-  var num1 = parseFloat(document.getElementById('firstVal').value)
-  var num2 = parseFloat(document.getElementById('secondVal').value)
+  var num1 = parseFloat(document.getElementById("firstVal").value)
+  var num2 = parseFloat(document.getElementById("secondVal").value)
 
   var sum = num1 / num2
   //user sees result
   //display
 
-  document.getElementById('result').innerHTML = sum
+  document.getElementById("result").innerHTML = sum
 
-  document.getElementById('firstVal').innerHTML = ""
-  document.getElementById('secondVal').innerHTML = ""
-
+  document.getElementById("firstVal").innerHTML = ""
+  document.getElementById("secondVal").innerHTML = ""
 }
+
+//                         Moment.js
+var Now = moment().format("MMMM Do YYYY, h:mm:ss a")
+var x = document.getElementById("msgdiv")
+x.innerHTML = Now
